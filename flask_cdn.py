67bc5769100b8fea4e86bea020b77407edaa9ff3
Bernardo Heynemann <heynemann@gmail.com>
2014-09-12 15:19:11 -0300

    blueprint only overrides if static_folder is defined

--- flask_cdn.py
+++ flask_cdn.py
@@ -30,7 +30,7 @@ def url_for(endpoint, **values):
             scheme = 'https'
 
         static_folder = app.static_folder
-        if request.blueprint is not None:
+        if request.blueprint is not None and app.blueprints[request.blueprint].static_folder:
             static_folder = app.blueprints[request.blueprint].static_folder
 
         cdn_domain = app.config['CDN_DOMAIN']
